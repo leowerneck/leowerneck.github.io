f62ec0686c78ad712543e9f43277ad6c68a64a18
Leo Werneck <wernecklr@gmail.com>
2025-06-10 10:58:40 -0500

    Fixed issues with presentation layout and plugin

--- _plugins/preprocess_slide_content.rb
+++ _plugins/preprocess_slide_content.rb
@@ -1,36 +1,25 @@
 module Jekyll
   class PreprocessedSlides < Generator
     def generate(site)
-      site.posts.docs.each { |post| post.data['preprocessed_slide_content'] = preprocess(post.content) }
-      site.pages.each { |page| page.data['preprocessed_slide_content'] = preprocess(page.content) }
-      site.collections.each do |_, collection|
-        collection.docs.each { |doc| doc.data['preprocessed_slide_content'] = preprocess(doc.content) }
+      site.pages.each do |page|
+        next unless page.data["layout"] == "slides"
+
+        original = page.content.dup
+        page.data["preprocessed_slide_content"] = preprocess(original)
+
+        Jekyll.logger.info "--- BEGIN ---"
+        Jekyll.logger.info page.data["preprocessed_slide_content"] # This will print the multi-line string
+        Jekyll.logger.info "--- END ---"
       end
     end
 
     private
 
     def preprocess(content)
-      content = preprocess_title_slide(content)
       content = preprocess_equations(content)
-      content = preprocess_lists(content)
       content = preprocess_titles(content)
+      content = preprocess_lists(content)
       content
-    end
-
-    def preprocess_title_slide(content)
-      lines = content.strip.split("\n")
-      first_slide_lines = ["<!--begin title slide-->"]
-      n = 0
-      lines.each_with_index do |line, i|
-        if line.strip == "<!--s-->"
-          n = i
-          break
-        end
-        first_slide_lines << line
-      end
-      first_slide_lines << "<!--end title slide-->"
-      (first_slide_lines + lines[n..]).join("\n")
     end
 
     def preprocess_equations(content)
